7d06c468a56e79eb616cc8e399113e263b21446c
d3noDL <dino@adenogame.com>
2023-09-15 19:53:34 +0200

    Custom data types

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "raylib.h"
 
+struct AnimationData {
+    Rectangle rect;
+    Vector2 position;
+    int frame;
+    float update_time;
+    float running_time;
+};
+
 int main() {
     const int WINDOW_WIDTH{512};
     const int WINDOW_HEIGHT{380};
@@ -12,36 +20,33 @@ int main() {
     
     // Nebula variables
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-    Rectangle nebula_rec{0.0, 0.0, nebula.width/8, nebula.height/8};
-    Vector2 nebula_pos{WINDOW_WIDTH, WINDOW_HEIGHT - nebula_rec.height};
+    AnimationData nebula_data {
+        rect: {0.0, 0.0, nebula.width/8, nebula.height/8},
+        position: {WINDOW_WIDTH, WINDOW_HEIGHT - nebula.height/8},
+        frame: {0},
+        update_time: {1.0/12.0},
+        running_time: {0}
+    };
 
     // Nebula X velocity (pixels/second)
-    int nebula_velocity{-200};
+    
 
     // Scarfy variables
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
-    Rectangle scarfy_rec;
-        scarfy_rec.width = scarfy.width/6;
-        scarfy_rec.height = scarfy.height;
-        scarfy_rec.x = 0;
-        scarfy_rec.y = 0;
-    Vector2 scarfy_pos;
-        scarfy_pos.x = WINDOW_WIDTH/2 - scarfy_rec.width/2;
-        scarfy_pos.y = WINDOW_HEIGHT - scarfy_rec.height;
+    AnimationData scarfy_data {
+        rect: {scarfy.width/6, scarfy.height, 0, 0},
+        position: {WINDOW_WIDTH/2 - scarfy.width/2, WINDOW_HEIGHT - scarfy.height},
+        frame: {0},
+        update_time: {1.0/12.0},
+        running_time: {0}
+    };
     
-    // Animation frame
-    int frame{};
-    // Amount of time before we update the animation frame
-    const float UPDATE_TIME{1.0/12.0};
-    float running_time{};
-
-    int nebula_frame{};
-    const float NEBULA_UPDATE_TIME{1.0/12.0};
-    float nebula_running_time{};
+    
 
     bool is_grounded{true};
     
     int velocity{0};
+    int nebula_velocity{-200};
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
@@ -53,7 +58,7 @@ int main() {
         const float DT{GetFrameTime()};
 
         // Check if on ground.
-        if (scarfy_pos.y >= WINDOW_HEIGHT - scarfy_rec.height) {
+        if (scarfy_data.position.y >= WINDOW_HEIGHT - scarfy_data.rect.height) {
             // Is on ground
             velocity = 0;
             is_grounded = true;
@@ -71,46 +76,46 @@ int main() {
         
         
         // Update nebula position.
-        nebula_pos.x += nebula_velocity * DT;
+        nebula_data.position.x += nebula_velocity * DT;
 
 
         // Update scarfy position.
-        scarfy_pos.y += velocity * DT;
+        scarfy_data.position.y += velocity * DT;
 
         // Update scarfy anim frame
         if (is_grounded) {
             // Update running time.
-            running_time += DT;
-            if (running_time >= UPDATE_TIME) {
-                running_time = 0.0;
+            scarfy_data.running_time += DT;
+            if (scarfy_data.running_time >= scarfy_data.update_time) {
+                scarfy_data.running_time = 0.0;
 
                 // Update animation frame.
-                scarfy_rec.x = frame * scarfy_rec.width;
-                frame++;
+                scarfy_data.rect.x = scarfy_data.frame * scarfy_data.rect.width;
+                scarfy_data.frame++;
 
-                if (frame > 5) {
-                    frame = 0;
+                if (scarfy_data.frame > 5) {
+                    scarfy_data.frame = 0;
                 }
             }
         }
 
         // Update nebula anim frame
-        nebula_running_time += DT;
-        if (nebula_running_time >= NEBULA_UPDATE_TIME) {
-            nebula_running_time = 0.0;
-            nebula_rec.x = nebula_frame * nebula_rec.width;
-            nebula_frame++;
+        nebula_data.running_time += DT;
+        if (nebula_data.running_time >= nebula_data.update_time) {
+            nebula_data.running_time = 0.0;
+            nebula_data.rect.x = nebula_data.frame * nebula_data.rect.width;
+            nebula_data.frame++;
 
-            if (nebula_frame > 7) {
-                nebula_frame = 0;
+            if (nebula_data.frame > 7) {
+                nebula_data.frame = 0;
             }
         }
         
         // Draw nebula
-        DrawTextureRec(nebula, nebula_rec, nebula_pos, WHITE);
+        DrawTextureRec(nebula, nebula_data.rect, nebula_data.position, WHITE);
         
         // Draw scarfy
-        DrawTextureRec(scarfy, scarfy_rec, scarfy_pos, WHITE);
+        DrawTextureRec(scarfy, scarfy_data.rect, scarfy_data.position, WHITE);
 
         /* END LOGIC */
         EndDrawing();
