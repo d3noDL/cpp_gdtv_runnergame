ef5548a31ea6a0f4586382c1b3099afe5a719fed
d3noDL <dino@adenogame.com>
2023-09-15 13:48:09 +0200

    Velocity and Jumping

--- main.cpp
+++ main.cpp
@@ -5,14 +5,27 @@ int main() {
     const int WINDOW_HEIGHT{380};
     
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Runner Game");
-    SetTargetFPS(60);
     
+    // Rectangle directions
+    const int REC_WIDTH{50};
+    const int REC_HEIGHT{80};
+
+    int pos_y{WINDOW_HEIGHT - REC_HEIGHT};
+    int velocity{0};
+
+    SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
         /* BEGIN LOGIC */
         
-        
+        if (IsKeyPressed(KEY_SPACE)) {
+            velocity -= 10;
+        }
+
+        pos_y += velocity;
+
+        DrawRectangle(WINDOW_WIDTH/2, pos_y, REC_WIDTH, REC_HEIGHT, BLUE);
 
         /* END LOGIC */
         EndDrawing();
